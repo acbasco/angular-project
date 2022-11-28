969c819855edeadbe9082db2c8f7210e5c4fcebe
Angelo Basco <bascoangelo.lns@gmail.com>
2022-11-28 14:00:11 +0800

    Fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {LoginComponent} from './core/components/login/login.component';
-import {RegisterComponent} from './core/components/register/register.component';
-import {UsersComponent} from './core/components/users/users.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
@@ -12,9 +9,6 @@ import {FormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    UsersComponent
   ],
   imports: [
     BrowserModule,
